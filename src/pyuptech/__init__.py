59151408bc0c97ae50d7830ff1f69fd527b846e5
Whth <88489697+Whth@users.noreply.github.com>
2024-04-18 18:20:01 +0800

    refactor(move): move io pins utility to pins.py

--- src/pyuptech/__init__.py
+++ src/pyuptech/__init__.py
@@ -1,12 +1,13 @@
 from .modules.loader import load_lib
 from .modules.logger import set_log_level
-
-from .modules.sensors import (
-    OnBoardSensors,
+from .modules.pins import (
     pin_setter_constructor,
+    pin_getter_constructor,
     pin_mode_setter_constructor,
     multiple_pin_mode_setter_constructor,
-    pin_getter_constructor,
+)
+from .modules.sensors import (
+    OnBoardSensors,
 )
 
 __all__ = [
--- src/pyuptech/modules/sensors.py
+++ src/pyuptech/modules/sensors.py
@@ -1,19 +1,12 @@
 import ctypes
 from time import perf_counter_ns
-from typing import Callable, Sequence, Self
+from typing import Self
 
 from .constant import LIB_FILE_PATH
 from .loader import _logger
 from .loader import load_lib
 
 E6 = 1000000
-
-PinModeSetter = Callable[[int], None]
-PinSetter = Callable[[int], None]
-PinGetter = Callable[[], int]
-
-IndexedGetter = Callable[[int], int]
-IndexedSetter = Callable[[int, int], None]
 
 OUTPUT = 1
 INPUT = 0
@@ -298,79 +291,3 @@ class OnBoardSensors:
     @staticmethod
     def get_handle(attr_name: str):
         return getattr(OnBoardSensors.__lib, attr_name)
-
-
-def pin_setter_constructor(indexed_setter: IndexedSetter, pin: int) -> PinSetter:
-    """
-
-    Args:
-        indexed_setter: the function that
-        pin: the pin to be connected
-
-    Returns:
-
-    """
-
-    def set_pin_level(level: int):
-        indexed_setter(pin, level)
-
-    return set_pin_level
-
-
-def pin_getter_constructor(indexed_getter: IndexedGetter, pin: int) -> PinGetter:
-    """
-
-    Args:
-        indexed_getter:
-        pin:
-
-    Returns:
-
-    """
-
-    def get_pin_level() -> int:
-        return indexed_getter(pin)
-
-    return get_pin_level
-
-
-def pin_mode_setter_constructor(
-    indexed_mode_setter: IndexedSetter, pin: int
-) -> PinModeSetter:
-    """
-
-    Args:
-        indexed_mode_setter: the function that sets the pin mode
-        pin: the pin to be connected
-
-    Returns:
-        the function that sets the pin mode with built-in pin value
-
-    """
-
-    def set_pin_mode(mode: int):
-        indexed_mode_setter(pin, mode)
-
-    return set_pin_mode
-
-
-def multiple_pin_mode_setter_constructor(
-    indexed_mode_setter: IndexedSetter, pins: Sequence[int]
-) -> PinModeSetter:
-    """
-
-    Args:
-        pins: the pin to be connected
-        indexed_mode_setter: the function that sets the pin mode
-
-
-    Returns:
-        the function that sets the pin mode with built-in pin value
-
-    """
-
-    def set_pin_mode(mode: int):
-        for pin in pins:
-            indexed_mode_setter(pin, mode)
-
-    return set_pin_mode
